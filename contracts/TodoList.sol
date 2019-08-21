6422538b08c5ce2e121d3daff348e068614bf9fc
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2019-08-21 08:56:54 -0500

    Created a todo list in TodoList.sol

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-  uint taskCount = 0; // "state variable"
+
+  constructor () public {
+    createTask("Check out dappuniversity.com");
+  }
+  
+  uint public taskCount = 0; // "state variable"
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping (uint => Task) public tasks;
+
+  function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
 }
